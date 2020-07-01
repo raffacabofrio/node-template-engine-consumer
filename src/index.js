fa3ae413d031f5c08386994cf7878cd7bf79ac81
Raffaello Damgaard <raffacabofrio@gmail.com>
2020-07-01 04:25:10 -0300

    Fix vs code settings.

--- src/index.js
+++ src/index.js
@@ -7,11 +7,5 @@ app.get('/', function (req, res) {
   res.send(html)
 })
 
-// app.post('/business-card', function (req, res) {
-//   // var html = require('./templates/form.chop')({ name: 'raffa' })
-//   // console.log(html)
-//   // res.send('hello')
-// })
-
 console.log('app running and listening on http://localhost:3000')
 app.listen(3000)
