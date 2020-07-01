c829fcec6a9359597ef06f3b2106da3f467ced7a
Raffaello Damgaard <raffacabofrio@gmail.com>
2020-07-01 04:23:25 -0300

    Endpoint using chop template engine.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+require('chop-template-engine')
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  var html = require('./templates/form.chop')({ name: 'raffa', age: 41 })
+  res.send(html)
 })
+
+// app.post('/business-card', function (req, res) {
+//   // var html = require('./templates/form.chop')({ name: 'raffa' })
+//   // console.log(html)
+//   // res.send('hello')
+// })
 
 console.log('app running and listening on http://localhost:3000')
 app.listen(3000)
